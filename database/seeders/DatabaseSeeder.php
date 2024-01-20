a6e0487524247d87e36cac8048549cd785a721c2
José Antonio Rosales <jose.rosales.mald@gmail.com>
2024-01-19 21:00:49 -0600

    New admin column and seeder for admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,12 +13,16 @@ class DatabaseSeeder extends Seeder
    */
   public function run(): void
   {
-    \App\Models\User::factory(10)->create();
+    // \App\Models\User::factory(10)->create();
 
-    \App\Models\User::factory()->create([
-        'name' => 'Test User',
-        'email' => 'test@example.com',
-        'password' => bcrypt('1234'),
+    $user = User::create([
+      'name' => 'admin',
+      'email' => 'admin@gmail.com',
+      'password' => '87654321',
+      'is_admin' => 1,
     ]);
+
+    $user->save();
   }
+
 }
